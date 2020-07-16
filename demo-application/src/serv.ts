396d8aaeb037ffa35dea76ab443b40b708574525
jse41 <54693948+jse41@users.noreply.github.com>
2020-07-16 10:28:05 -0400

    Add Update Functionality
    Updates to blockchain API to allow external updates

--- demo-application/src/serv.ts
+++ demo-application/src/serv.ts
@@ -7,7 +7,7 @@ var express = require('express');
 var app = express()
 app.use(bodyParse.json())
 
-app.get('/api/queryallcars/:id', async function (req: any, res: any) {
+app.get('/api/get/:id', async function (req: any, res: any) {
     try {
 
         // Create a new file system based wallet for managing identities.
@@ -33,6 +33,74 @@ app.get('/api/queryallcars/:id', async function (req: any, res: any) {
         const result = await contract.evaluateTransaction('readMyAsset', req.params.id);
         console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
         res.status(200).json({response: result.toString()});
+
+    } catch (error) {
+        console.error(`Failed to evaluate transaction: ${error}`);
+        res.status(500).json({error: error});
+        process.exit(1);
+    }
+});
+
+app.get('/api/set/:id/:newval', async function (req: any, res: any) {
+    try {
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'Org1Wallet');
+        const wallet = new FileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        const connectionProfile = path.resolve(__dirname, '..', 'connection.json');
+        let connectionOptions = { wallet, identity: 'org1Admin', discovery: { enabled: true, asLocalhost: true }};
+        await gateway.connect(connectionProfile, connectionOptions);
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network.
+        const contract = network.getContract('demo-contract');
+
+        // Evaluate the specified transaction.
+        // queryCar transaction - requires 1 argument, ex: ('queryCar', 'CAR4')
+        // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
+        await contract.submitTransaction('createMyAsset', req.params.id, req.params.newval);
+        console.log(`Transaction has been submitted.`);
+        res.status(200).json({response: `Transaction has been submitted.`});
+
+    } catch (error) {
+        console.error(`Failed to evaluate transaction: ${error}`);
+        res.status(500).json({error: error});
+        process.exit(1);
+    }
+});
+
+app.get('/api/set/:update/:newval', async function (req: any, res: any) {
+    try {
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'Org1Wallet');
+        const wallet = new FileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        const connectionProfile = path.resolve(__dirname, '..', 'connection.json');
+        let connectionOptions = { wallet, identity: 'org1Admin', discovery: { enabled: true, asLocalhost: true }};
+        await gateway.connect(connectionProfile, connectionOptions);
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network.
+        const contract = network.getContract('demo-contract');
+
+        // Evaluate the specified transaction.
+        // queryCar transaction - requires 1 argument, ex: ('queryCar', 'CAR4')
+        // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
+        await contract.submitTransaction('updateMyAsset', req.params.id, req.params.newval);
+        console.log(`Transaction has been updated.`);
+        res.status(200).json({response: `Transaction has been updated.`});
 
     } catch (error) {
         console.error(`Failed to evaluate transaction: ${error}`);
